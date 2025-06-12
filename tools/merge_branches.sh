03a064b46fdca393e8e8ca8a73970bbd33f2f0bd
jaxolv <jackson.oliveira.dsa@gmail.com>
2025-06-12 15:13:36 -0300

    Criação de script para merge em outras branches após mudanças criadas em "stage"

---
+++ tools/merge_branches.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+current_branch=$(git branch --show-current)
+if [ "$current_branch" != "stage" ]; then
+  git checkout stage || { echo "Falha ao mudar para a branch 'stage'. Mude manualmente:"; exit 1; }
+
+  echo "Copie e cole: git checkout stage"
+
+  exit 1
+fi
+
+branches=("sql" "scripts" "dashboards")
+
+for branch in "${branches[@]}ss"
+do
+    git checkout $branch || { echo "Falha ao mudar para $branch"; exit 1; }
+
+    git merge stage --no-edit || { echo "Conflito ao fazer merge em $branch. Resolva manualmente."; exit 1; }
+
+    git push origin $branch || { echo "Push falhou para $branch"; exit 1; }
+done
+
+git checkout stage

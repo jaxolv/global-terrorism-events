4b316a18aaa0f7f929b26d573ea09427f4c4a2bb
jaxolv <jackson.oliveira.dsa@gmail.com>
2025-06-14 16:02:23 -0300

    Simplificando a query da questão 41, tornando-a menos redundante e mais clara

--- sql/questao_41.sql
+++ sql/questao_41.sql
@@ -2,33 +2,22 @@
 -- (número total de tipos diferentes de ataques por país) x (percentual fracionário médio de ataques bem-sucedidos na região)
 -- Liste as regiões com seus índices, ordenadas do maior para o menor.
 
-with tab_somatorio_variedade_ataques_regiao as (
+with sub_paises_tipos_ataques as (
     select
+        country_id,
         region_id,
-        sum(quant_ataques_tipo_pais) as somatorio_variedades_ataques_paises
-    from (
-        select
-            country_id,
-            region_id,
-            count(distinct attack_id) as quant_ataques_tipo_pais
-        from terrorism_act
-        where attack_id is not null
-        group by country_id, region_id
-    ) as sub_paises_tipos_ataques
-    group by region_id
+        count(distinct attack_id) as quant_ataques_tipo_pais,
+        sum(case when success then 1 else 0 end)::numeric / count(*) as percentual_ataques_bem_sucedidos
+    from terrorism_act
+    where attack_id is not null
+    group by country_id, region_id
 ),
-tab_media_percentuais_ataques_bem_sucedidos_regiao as (
+tab_somatorio_variedade_ataques_regiao as (
     select
         region_id,
+        sum(quant_ataques_tipo_pais) as somatorio_variedades_ataques_paises,
         avg(percentual_ataques_bem_sucedidos) as media_ataques_bem_sucedidos_regiao
-    from (
-        select
-            country_id,
-            region_id,
-            sum(case when success then 1 else 0 end)::numeric / count(*) as percentual_ataques_bem_sucedidos
-        from terrorism_act
-        group by country_id, region_id
-    ) as sub_percentuais_ataques_bem_sucedidos_pais
+    from sub_paises_tipos_ataques
     group by region_id
 )
 select
@@ -36,7 +25,6 @@ select
     round(
         media_ataques_bem_sucedidos_regiao * somatorio_variedades_ataques_paises, 2
     ) as indice_de_instabilidade
-from tab_media_percentuais_ataques_bem_sucedidos_regiao mpr
-join tab_somatorio_variedade_ataques_regiao sva on mpr.region_id = sva.region_id
-join region r on r.id = mpr.region_id
+from tab_somatorio_variedade_ataques_regiao sva
+join region r on r.id = sva.region_id
 order by indice_de_instabilidade desc;
